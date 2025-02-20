3d320095e113bb1fda90962c94247d36504b3dd0
RaffDev31 <raffli4000@hotmail.com>
2025-02-20 22:09:55 +0700

    feat(view): enhance settings page to include bank account information

--- resources/views/admin/pengaturan/index.blade.php
+++ resources/views/admin/pengaturan/index.blade.php
@@ -14,6 +14,7 @@
             <div class="card-body">
                 <form action="{{ route('admin.setting.simpan') }}" method="post" enctype="multipart/form-data">
                     @csrf
+                    <!-- About Section -->
                     <div class="form-group">
                         <label for="">About</label>
                         <textarea name="about"
@@ -24,6 +25,8 @@
                         <small class="text-danger">{{ $message }}</small>
                         @enderror
                     </div>
+
+                    <!-- Harga Akun Berbayar Section -->
                     <div class="form-group">
                         <label for="">Harga Akun Berbayar</label>
                         <input type="text" name="harga" class="form-control @error('harga') is-invalid @enderror"
@@ -32,6 +35,27 @@
                         <small class="text-danger">{{ $message }}</small>
                         @enderror
                     </div>
+
+                    <!-- No Rekening Section -->
+                    <div class="form-group">
+                        <label for="">No Rekening</label>
+                        <input type="text" name="no_rekening" class="form-control @error('no_rekening') is-invalid @enderror"
+                            value="{{ $rekening->no_rekening }}">
+                        @error('no_rekening')
+                        <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+
+                    <!-- Atas Nama Section -->
+                    <div class="form-group">
+                        <label for="">Atas Nama</label>
+                        <input type="text" name="atas_nama" class="form-control @error('atas_nama') is-invalid @enderror"
+                            value="{{ $rekening->atas_nama }}">
+                        @error('atas_nama')
+                        <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+
                     <div class="text-right">
                         <button type="submit" class="btn btn-success">Perbaharui Pengaturan</button>
                     </div>
