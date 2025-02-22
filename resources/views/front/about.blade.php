f742f7c68d8a62cb2bde32dbe9539602b80badc9
RaffDev31 <raffli4000@hotmail.com>
2025-02-23 00:23:59 +0700

    feat(about): add about section with team details and class types

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -7,14 +7,69 @@
           <div class="breadcrumb_iner text-center">
             <div class="breadcrumb_iner_item">
               <h2>About CerdasMudah</h2>
-              <p>Aplikasi ini dibuat oleh para siswa dan siswi SMKN 4 Kota Tangerang dikala sedang mempersiapkan ulangan-ulangan akhir semester.</p>
+              <p>Aplikasi ini dibuat oleh para siswa dan siswi SMKN 4 Kota Tangerang dikala sedang mempersiapkan
+                ulangan-ulangan akhir semester.</p>
             </div>
           </div>
         </div>
       </div>
     </div>
   </section>
+  <style>
+    .judultim {
+      text-align: center;
+      margin-top: 10px;
+    }
 
+    .garisbejir {
+      border-bottom: 2.5px solid #0c2e60;
+      padding-top: 10px;
+      width: 300px;
+      text-align: center;
+      margin: auto;
+    }
+
+    .teammuter {
+      display: flex;
+      justify-content: center;
+      gap: 20px;
+      flex-wrap: wrap;
+      margin-top: 30px;
+    }
+
+    .isiTeam {
+      flex: 1 1 300px;
+      max-width: 250px;
+    }
+
+    .card {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+
+    .isiTeam .card:hover {
+      transition: transform 0.5s ease-in-out;
+      transform: scale(1.08);
+      border: 1px solid #415094;
+    }
+
+    .card-body {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      text-align: center;
+    }
+
+    .card-body img {
+      width: 150px;
+      height: 150px;
+      object-fit: cover;
+      border-radius: 50%;
+      margin: 0 auto;
+      margin-bottom: 10px;
+    }
+  </style>
   <!-- feature_part start-->
   <section class="feature_part">
     <div class="container">
@@ -26,7 +81,7 @@
           </div>
         </div>
         <div class="col-sm-6 col-xl-3">
-          <div class="single_feature shadow">
+          <div class="single_feature">
             <div class="single_feature_part">
               <span class="single_feature_icon"><i class="ti-layers"></i></span>
               <h4>Gratis</h4>
@@ -35,7 +90,7 @@
           </div>
         </div>
         <div class="col-sm-6 col-xl-3">
-          <div class="single_feature shadow">
+          <div class="single_feature">
             <div class="single_feature_part">
               <span class="single_feature_icon"><i class="ti-new-window"></i></span>
               <h4>Regular</h4>
@@ -44,11 +99,12 @@
           </div>
         </div>
         <div class="col-sm-6 col-xl-3">
-          <div class="single_feature shadow">
+          <div class="single_feature">
             <div class="single_feature_part single_feature_part_2">
               <span class="single_service_icon style_icon"><i class="ti-light-bulb"></i></span>
               <h4>Premium</h4>
-              <p>Kelas Premium hanya bisa diakses dengan cara membuat akun dan mengupgrade akun ke akun premium</p>
+              <p>Kelas Premium hanya bisa diakses dengan cara membuat akun dan mengupgrade akun ke akun
+                premium</p>
             </div>
           </div>
         </div>
@@ -56,12 +112,10 @@
     </div>
   </section>
   <!-- upcoming_event part start-->
-
   <div class="garisbejir"></div>
   <div class="judultim">
     <h3>Our Team</h3>
   </div>
-
   <!-- learning part start-->
   <div class="teammuter">
     <div class="isiTeam">
@@ -76,9 +130,9 @@
     <div class="isiTeam">
       <div class="card">
         <div class="card-body">
-          <img src="{{ URL::asset('frontemplate/img/team/raplifoto.png') }}">
+          <img src="{{ URL::asset('frontemplate/img/team/rafflipoto.png') }}">
           <h5>Raffli Firmansyah</h5>
-          <p>Asst. Manager / Full Stack Developer</p>
+          <p>Asst. Manager / Fullstack Developer</p>
         </div>
       </div>
     </div>
@@ -109,9 +163,9 @@
         </div>
       </div>
     </div>
-</div>
-
-
+  </div>
+  {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/micro-slider@1.0.9/dist/micro-slider.min.js"></script> --}}
   <script></script>
   <!-- learning part end-->
 @endsection
