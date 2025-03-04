e8343c99b9abc249f5484bf6176a57ce33679eca
RaffDev31 <raffli4000@hotmail.com>
2025-03-04 17:42:22 +0700

    feat: add Font Awesome for enhanced icon support in the header

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -11,6 +11,8 @@
   <link rel="stylesheet" href="{{ asset('frontemplate') }}/css/bootstrap.min.css">
   <!-- animate CSS -->
   <link rel="stylesheet" href="{{ asset('frontemplate') }}/css/animate.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- owl carousel CSS -->
   <link rel="stylesheet" href="{{ asset('frontemplate') }}/css/owl.carousel.min.css">
   <!-- themify CSS -->
